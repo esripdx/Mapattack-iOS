8a85f9d9efce38e1cdc81735646e6426324654f6
Jen Oslislo <poeksweb@gmail.com>
2013-09-18 15:25:57 -0700

    Some storyboard stub classes

---
+++ GLCreateViewController.h
@@ -0,0 +1,13 @@
+//
+//  GLCreateViewController.h
+//  Mapattack-iOS
+//
+//  Created by Jen on 9/18/13.
+//  Copyright (c) 2013 Geoloqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLCreateViewController : UIViewController
+
+@end
---
+++ GLCreateViewController.m
@@ -0,0 +1,38 @@
+//
+//  GLCreateViewController.m
+//  Mapattack-iOS
+//
+//  Created by Jen on 9/18/13.
+//  Copyright (c) 2013 Geoloqi. All rights reserved.
+//
+
+#import "GLCreateViewController.h"
+
+@interface GLCreateViewController ()
+
+@end
+
+@implementation GLCreateViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
