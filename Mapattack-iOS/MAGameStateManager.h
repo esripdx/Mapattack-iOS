9421fae382af409cc738fb70a7f3bb8277056948
Ryan Arana <ryan.arana@gmail.com>
2013-09-19 14:00:22 -0700

    Put all UI classes into Interface Xcode Group, and wrangle a stray VC file.

---
+++ Mapattack-iOS/MAGameStateManager.h
@@ -0,0 +1,13 @@
+//
+//  MAGameStateManager.h
+//  Mapattack-iOS
+//
+//  Created by Ryan Arana on 9/19/13.
+//  Copyright (c) 2013 Geoloqi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MAGameStateManager : NSObject
+
+@end
---
+++ Mapattack-iOS/MAGameStateManager.m
@@ -0,0 +1,13 @@
+//
+//  MAGameStateManager.m
+//  Mapattack-iOS
+//
+//  Created by Ryan Arana on 9/19/13.
+//  Copyright (c) 2013 Geoloqi. All rights reserved.
+//
+
+#import "MAGameStateManager.h"
+
+@implementation MAGameStateManager
+
+@end
